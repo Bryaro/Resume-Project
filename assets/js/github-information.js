a52aabc14abf7d27ae0b549f7c3a5c211cab6a08
Bryar Osman <bryarosman.bo@gmail.com>
2024-01-14 19:25:16 +0000

    fix bug for gh.user-data and gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,6 +37,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -48,7 +50,7 @@ function fetchGitHubInformation(event) {
         `<div id="loader">
             <img src="assets/css/loading.gif" alt="loading..." />
         </div>`);
-
+    
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`),
         $.getJSON(`https://api.github.com/users/${username}/repos`)
@@ -70,3 +72,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
